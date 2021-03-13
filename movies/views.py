e94f8de45d41e1c7a8038db780645b0752f854bc
soc4585 <soc4585@gmail.com>
2021-03-13 09:51:02 +0900

    pjt04 완성

--- movies/views.py
+++ movies/views.py
@@ -16,12 +16,45 @@ def create(request):
     title = request.POST.get('title')
     overview = request.POST.get('overview')
     poster_path = request.POST.get('poster_path')
-    Movie.objects.create(title=title, overview=overview, poster_path=poster_path)
-    return redirect('movies:index')
+    # Movie.objects.create(title=title, overview=overview, poster_path=poster_path)
+    # return redirect('movies:index')
+    movie = Movie(title=title, overview=overview, poster_path=poster_path)
+    movie.save()
+    return redirect('movies:detail', movie.pk)
+
 
 def detail(request, pk):
     movie = Movie.objects.get(pk=pk)
     context = {
         'movie': movie,
     }
-    return render(request, 'movies.detail.html', context)
+    return render(request, 'movies/detail.html', context)
+
+def delete(request, pk):
+    if request.method == 'POST':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return redirect('movies:index')
+    else:
+        return redirect('movies:detail', pk)
+
+def edit(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    context = {
+        'movie':movie
+    }
+    return render(request, 'movies/edit.html', context)
+
+def update(request, pk):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        overview = request.POST.get('overview')
+        poster_path = request.POST.get('poster_path')
+
+        movie = Movie.objects.get(pk=pk)
+        movie.title = title
+        movie.overview = overview
+        movie.poster_path = poster_path
+        movie.save()
+        return redirect('movies:detail', pk)
+    return redirect('movies:edit', pk)
